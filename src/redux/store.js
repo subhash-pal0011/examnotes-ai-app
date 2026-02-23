741b070a4ee53511f024223271df5af2af8082f8
subhash pal <palsubhash046@gmail.com>
2026-02-23 10:18:09 +0530

    add notesSlice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import userSlice  from "./userSlice"
+import notesSlice  from './GetNotes'
 
 export const store = configureStore({
        reducer:{
-              user:userSlice
+              user:userSlice,
+              notes:notesSlice
        }
 })
